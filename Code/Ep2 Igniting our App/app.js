ebf57ada26ab4cdcaeb33165d86a18548574403b
Bharat Kumar <bharatkumar@Bharats-MacBook-Air.local>
2025-03-18 21:08:49 +0530

    Ep2

--- Code/Ep2 Igniting our App/app.js
+++ Code/Ep2 Igniting our App/app.js
@@ -1,14 +1,12 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom/client'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const heading1 = React.createElement("h1", {id:"heading1"},"Hello nam kese ");
-
+const heading1 = React.createElement("h1", {id:"heading1"},"Hello bhai kese ho");
 
 const child1 = React.createElement("div",{id:"child1"},heading1);
 
 const parent  = React.createElement("div",{id:"parent"},child1);
 
-console.log(parent);
 root.render(parent);
