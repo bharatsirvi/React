adfff474e1fe12948c3435c702acdca76f60af53
Bharat Kumar <bharatkumar@Bharats-MacBook-Air.local>
2025-03-20 22:10:12 +0530

    Ep7

---
+++ Code/Ep7 Routing/src/App.js
@@ -0,0 +1,49 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./components/Header";
+import Body from "./components/Body";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router";
+import About from "./components/About";
+import Contact from "./components/Contect";
+import Error from "./components/Error";
+import RestaurantMenu from "./components/RestaurantMenu";
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Outlet/>
+    </div>
+  );
+};
+
+const appRoute = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children:[
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/restaurants/:resId",
+        element: <RestaurantMenu />,
+      },
+    ],
+    errorElement: <Error />,
+  },
+ 
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<RouterProvider router={appRoute} />);
---
+++ Code/Ep7 Routing/src/utils/constant.js
@@ -0,0 +1,8 @@
+export const LOGO_URL =
+  "https://img.freepik.com/premium-vector/premium-logo-template-2025_1271687-73.jpg?w=1380";
+export const RESTAURANT_IMAGES_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+export const SWIGGY_API_URL =
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.1244669&lng=75.8098141&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+
+export const MENU_API_URL= "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=25.1244669&lng=75.8098141&restaurantId=" 
